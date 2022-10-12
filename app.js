dd5ca0b6af470653b881db898545605ab4d0fde7
AllanSam <93815162+AllanSam-codes@users.noreply.github.com>
2022-10-13 00:18:52 +0530

    addded tabel at end

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-const bilAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cah-given")
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given")
 const checkButton = document.querySelector("#check-button")
 const Message = document.querySelector("#error-message");
+const noOfNotes = document.querySelector(".no-of-notes")
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
 
 //checkButton.addEventListener("click", () => console.log("clicked !"))
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage();
+
+    if (billAmount.value > 0) {
+        if (cashGiven.value > billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+
+        } else {
+            showMessage("The cash provide should be atleast equal to the bill amount")
+        }
+    } else {
+        showMessage("Invalid Bill amount ");
+    }
+
+});
+
+function calculateChange(amountToBeReturned) {
+
+    for (var i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(
+            amountToBeReturned / availableNotes[i]
+        );
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes
+    }
+
+}
+
+function hideMessage() {
     Message.style.display = "none";
 
-    if (bilAmount.value > 0) {
+}
 
-    } else {
-        Message.style.display = "block"
-        Message.innerText = "This bill amount should be greater than 0"
-
-    }
-})
+function showMessage(msg) {
+    Message.style.display = "block"
+    Message.innerText = msg
+}
