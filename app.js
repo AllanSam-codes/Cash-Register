a95e5560baf0ad1ff22d5cff23830487b81c012f
AllanSam <93815162+AllanSam-codes@users.noreply.github.com>
2022-10-13 15:52:33 +0530

    test

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
-
     if (billAmount.value > 0) {
         if (cashGiven.value > billAmount.value) {
             const amountToBeReturned = cashGiven.value - billAmount.value;
@@ -26,12 +25,12 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
 });
 
 function calculateChange(amountToBeReturned) {
-
     for (var i = 0; i < availableNotes.length; i++) {
         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
         amountToBeReturned = amountToBeReturned % availableNotes[i];
         noOfNotes[i].innerText = numberOfNotes
     }
+
 
 }
 
